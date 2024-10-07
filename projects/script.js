5e273738ccc1e293445d21636c1fade62aa17e77
Luke <lukegamertsfelder@icloud.com>
2024-10-07 09:42:35 -0500

    values update

--- projects/script.js
+++ projects/script.js
@@ -1,21 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let prevScrollPos = window.scrollY;
-    const navbar = document.getElementById('navbar');
-    const navbarHeight = navbar.offsetHeight;
-  
-    window.addEventListener('scroll', function() {
-      const currentScrollPos = window.scrollY;
-      
-      if (prevScrollPos > currentScrollPos) {
+  let prevScrollPos = window.scrollY;
+  const navbar = document.getElementById('navbar');
+  const navbarHeight = navbar.offsetHeight;
+  const scrollThreshold = 5; // scroll sensitivity 
 
-        navbar.classList.remove('hidden');
-      } else {
+  let hideTimeout;
 
-        if (currentScrollPos > navbarHeight) {
+  window.addEventListener('scroll', function() {
+    const currentScrollPos = window.scrollY;
+    const scrollDifference = prevScrollPos - currentScrollPos;
+    
+    clearTimeout(hideTimeout);
+
+    if (scrollDifference > scrollThreshold) {
+ 
+      navbar.classList.remove('hidden');
+    } else if (scrollDifference < 0) {
+
+      if (currentScrollPos > navbarHeight) {
+        hideTimeout = setTimeout(() => {
           navbar.classList.add('hidden');
-        }
+        }, 5); // delay
       }
-      
-      prevScrollPos = currentScrollPos;
-    });
+    }
+    
+    prevScrollPos = currentScrollPos;
   });
+});
